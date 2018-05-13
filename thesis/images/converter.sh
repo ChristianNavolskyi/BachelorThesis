69de9a00df7b488fa993aaa0e23736453479a090
ChristianNavolskyi <c.navolskyi@gmail.com>
2018-05-13 04:41:36 +0200

    Wrote last chapter.

--- thesis/images/converter.sh
+++ thesis/images/converter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for file in $(find . -type f -name '*.svg'); do
-  inkscape -D -z --file=$(pwd)/${file} --export-pdf=$(pwd)/${file%.*}.pdf
+  inkscape --file=$(pwd)/${file} --export-pdf=$(pwd)/${file%.*}.pdf
 done
